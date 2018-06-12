178343f7f01ed393a2f3674e70a3f5f7c2b8de62
alechp <me@alechp.com>
2018-06-12 11:11:14 -0700

    Initialized repo with 3 projects via repogen

---
+++ .repogen.js
@@ -0,0 +1,11 @@
+const config = {
+  provider: "alechp",
+  repospacePath: __dirname,
+  repositories: [
+    { servexyz: "docsify-starter" },
+    { servexyz: "prisma-authorized-starter" },
+    { servexyz: "aws-sls-auth-starter" }
+  ]
+};
+
+module.exports = { config };
